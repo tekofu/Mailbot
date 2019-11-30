ec2748728160b118198535f8f5c66d7114d04a6a
Ryan Fernandez <tekofu@users.noreply.github.com>
2019-11-30 21:42:38 +0000

    add owo and cry

--- Mailbot/main.py
+++ Mailbot/main.py
@@ -3,8 +3,10 @@
 
 import os
 import discord
-import owotrans
+from owotrans import owo
 import argparse
+import urllib.request
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("token", help="Discord bot token.")
@@ -20,9 +22,19 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('.owo '):
+        await message.channel.send(owo(message.content[4:]))
 
-client.run(parser.parse_args().input)
+    # uhhh
+    #elif message.content.upper.startswith('GOODNIGHT RYAN'):
+    #   await message.channel.send("Goodnight Ryan!")
+
+    elif message.content.startswith('.cry '):
+        usefulMsg = message.content[5:].replace(' ', '%20')
+        cryRequest = urllib.request.urlopen("https://api.apcry.deadbird.dev/cry/" + usefulMsg).read()
+        cryOutput = json.loads(cryRequest)
+        await message.channel.send(cryOutput['tears'])
+
+client.run(parser.parse_args().token)
 
 
