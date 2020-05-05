fb86c3de4c1feea4277d7281495376340b1137f8
tekofu <47366807+tekofu@users.noreply.github.com>
2020-05-05 08:29:23 +0100

    add dice roll command

--- Mailbot/main.py
+++ Mailbot/main.py
@@ -90,6 +90,18 @@ async def crowo(ctx, *, text):
     await ctx.send(cry.cry(combOutput))
 
 
+@bot.command()
+async def roll(ctx, dice:str):
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
 @bot.command(name='rps', description='Play Rock paper scissors with the bot')
 async def rpsCom(ctx, choice):
     usefulMsg = choice.upper()
